1de1d987d55cc93facbee1949b8781b266798662
uadypatil <uday1011patil@gmail.com>
2024-10-21 11:26:08 +0530

    Registration Apis updated in main.php controller

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -11,23 +11,23 @@ class Main extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model("MainModel");   
+        $this->load->model("MainModel");
     }
 
-  
+
     function Login()
     {
         if ($_SERVER['REQUEST_METHOD'] === "POST") {
-            $username=$this->input->post("username");
-            $password=$this->MainModel->encryptData($this->input->post("password"));
-            $role=$this->input->post("role");
-            $user=$this->MainModel->loginValidation($username,$password,$role);
-            if ($user!=null) {
+            $username = $this->input->post("username");
+            $password = $this->MainModel->encryptData($this->input->post("password"));
+            $role = $this->input->post("role");
+            $user = $this->MainModel->loginValidation($username, $password, $role);
+            if ($user != null) {
                 $this->output->set_status_header(200);
-                $response = array("status" => "Success","data"=>$user,"message" => "Login Successfull");
+                $response = array("status" => "Success", "data" => $user, "message" => "Login Successfull");
             } else {
                 $this->output->set_status_header(404);
-                $response = array("status" => "Error","message" => "Wrong Credentials");
+                $response = array("status" => "Error", "message" => "Wrong Credentials");
             }
         } else {
             $this->output->set_status_header(405);
@@ -35,27 +35,89 @@ class Main extends CI_Controller
         }
         $this->output->set_content_type("application/json")->set_output(json_encode($response));
     }
-    
 
-    // function Register()
-    // {
-    //     if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    //        $formdata=$this->input->post();
-    //         $role=$this->input->post("role");
-    //         // $user=$this->MainModel->loginValidation($username,$password,$role);
-    //         if ($user!=null) {
-    //             $this->output->set_status_header(200);
-    //             $response = array("status" => "Success","data"=>$user,"message" => "Login Successfull");
-    //         } else {
-    //             $this->output->set_status_header(404);
-    //             $response = array("status" => "Error","message" => "Wrong Credentials");
-    //         }
-    //     } else {
-    //         $this->output->set_status_header(405);
-    //         $response = array("status" => "error", "message" => "Bad Request");
-    //     }
-    //     $this->output->set_content_type("application/json")->set_output(json_encode($response));
-    // }
+    // function for registration
+    function registrationPost()
+    {
+        $returned = "";
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $formdata = $this->input->post();
+            $formdata['password'] = $this->MainModel->encryptData($formdata["password"]);
+
+            $authenticationdata = array(
+                'email' => $formdata['email'],
+                'contactno' => $formdata['contactno'],
+                'password' => $formdata['password'],
+                'type' => $formdata['type'],
+            );
+
+            $authid = $this->MainModel->AuthenticationPost($authenticationdata);
+            if ($authid != null) {
+                if ($authid == "contactno exist") {
+                    $this->output->set_status_header(404);
+                    $response = array("status" => "Error", "message" => "Contact Number Already Exist");
+                    $returned  = -1;
+                } else if ($authid == "email exist") {
+                    $this->output->set_status_header(404);
+                    $returned = -2;
+                } else {
+
+                    if ($formdata['type'] == "tailor") {
+                        // adding tailor data to array
+                        $tailordata = array(
+                            "authid" => $authid,
+                            "document_type" => $formdata['document_type'],
+                            "document" => $formdata['document'],
+                            "shop_license" => $formdata['shop_license'],
+                            "address" => $formdata['address']
+                        );
+
+                        $config['upload_path'] = 'uploads/tailor/documents';
+                        $config['allowed_types'] = 'gif|jpg|jpeg|png|pdf'; // Adjust as needed
+                        $config['max_size'] = 102400;
+                        $this->load->library('upload', $config);
+                        $this->upload->initialize($config);
+
+                        if (isset($_FILES["document"])) {
+                            if (!$this->upload->do_upload('document')) {
+                                $data['error'] = $this->upload->display_errors();
+                                // print_r($data);
+                            } else {
+                                $data['upload_data'] = $this->upload->data();
+                                $tailordata['document'] = $data['upload_data']['file_name'];
+                            }
+                        }
+
+                        $returned = $this->MainModel->setTailorDataPost($tailordata);
+                    } else if ($formdata['type'] == "user") {
+                        // adding userdata to array
+                        $userdata = array(
+                            "authid" => $authid,
+                            "name" => $formdata['name'],
+                            "address" => $formdata['address'],
+                        );
+
+                        $returned = $this->MainModel->setUserDataPost($userdata);
+                    }
+                }
+            }
+
+            // $user = $this->MainModel->loginValidation($username, $password, $role);
+            if($returned === -1){
+                $response = array("status" => "Error", "message" => "Contact number already exist");
+            }else if($returned === -2){
+                $response = array("status" => "Error", "message" => "Email already exist");
+            }else if ($returned != null) {
+                $this->output->set_status_header(200);
+                $response = array("status" => "Success", "message" => "Registration Successfull");
+            } else {
+                $this->output->set_status_header(404);
+                $response = array("status" => "Error", "message" => "Failed to Register " . $authid ." | " . $returned);
+            }
+        } else {
+            $this->output->set_status_header(405);
+            $response = array("status" => "error", "message" => "Bad Request");
+        }
+        $this->output->set_content_type("application/json")->set_output(json_encode($response));
+    }
 }
-
-?>
--- application/models/MainModel.php
+++ application/models/MainModel.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 
 defined("BASEPATH") or exit("No direct Script Access Allowed");
 
@@ -8,9 +8,8 @@ class MainModel extends CI_Model
     {
         parent::__construct();
         $this->load->database();
-
     }
- function encryptData($data)
+    function encryptData($data)
     {
         $iv = substr(hash('sha256', $data, true), 0, 16);
         $key = hash('sha256', $iv, true);
@@ -26,69 +25,104 @@ class MainModel extends CI_Model
         $decryptedData = openssl_decrypt($encryptedData, 'aes-256-cbc', $key, 0, $iv);
         return $decryptedData;
     }
-    function loginValidation($username,$password,$role)
+
+    // runction to add authentication details to database
+    function AuthenticationPost($formdata)
+    {
+        // Check for unique contact number
+        $this->db->where('contactno', $formdata['contactno']);
+        $query1 = $this->db->get('auth');
+
+        // Check for unique email
+        $this->db->where('email', $formdata['email']);
+        $query2 = $this->db->get('auth');
+
+        // If either contact number or email already exists, return an error message
+        if ($query1->num_rows() > 0) {
+            return "contactno exist";
+        } elseif ($query2->num_rows() > 0) {
+            return "email exist";
+        } else {
+            // Proceed with insert if unique
+            $status = $this->db->insert("auth", $formdata);
+            if ($status) {
+                $id = $this->db->insert_id();
+                return $id;
+            } else {
+                return null;
+            }
+        }
+
+    }   // function ends
+
+    // function to register the user
+    function setUserDataPost($userdata)
+    {
+        $status = $this->db->insert("user", $userdata);
+        return $status;
+    }   // function ends
+
+    // function to register the tailor
+    function setTailorDataPost($tailordata)
+    {
+        $status = $this->db->insert("tailor", $tailordata);
+        return $status;
+    }
+
+
+
+
+
+
+
+
+
+
+    function loginValidation($username, $password, $role)
     {
 
-        if ($role=="Admin") {
-            $Admin=$this->db->get_where("admin",array("username"=>$username,"password"=>$password))->row();
-            if($Admin!=null)
-            {
-                $Admin->role="Admin";
+        if ($role == "Admin") {
+            $Admin = $this->db->get_where("admin", array("username" => $username, "password" => $password))->row();
+            if ($Admin != null) {
+                $Admin->role = "Admin";
                 return $Admin;
-            }
-            else{
+            } else {
                 return null;
             }
-        } 
-        else if ($role=="School") {
-            $School=$this->db->get_where("school",array("username"=>$username,"password"=>$password))->row();
-            if($School!=null)
-            {
-                $School->role="School";
+        } else if ($role == "School") {
+            $School = $this->db->get_where("school", array("username" => $username, "password" => $password))->row();
+            if ($School != null) {
+                $School->role = "School";
                 return $School;
-            }
-            else{
+            } else {
                 return null;
             }
-        } 
-        else if ($role=="Staff") {
-            $Staff=$this->db->get_where("staff",array("username"=>$username,"password"=>$password))->row();
-            if($Staff!=null)
-            {
-                $Staff->role="Staff";
+        } else if ($role == "Staff") {
+            $Staff = $this->db->get_where("staff", array("username" => $username, "password" => $password))->row();
+            if ($Staff != null) {
+                $Staff->role = "Staff";
 
                 return $Staff;
-            }
-            else{
+            } else {
                 return null;
             }
-        } 
-        else if ($role=="Student") {
-            $Student=$this->db->get_where("student",array("username"=>$username,"password"=>$password))->row();
-            if($Student!=null)
-            {
-                $Student->role="Student";
+        } else if ($role == "Student") {
+            $Student = $this->db->get_where("student", array("username" => $username, "password" => $password))->row();
+            if ($Student != null) {
+                $Student->role = "Student";
                 return $Student;
-            }
-            else{
+            } else {
                 return null;
             }
-        } 
-        else {
-            $Class=$this->db->get_where("class",array("username"=>$username,"password"=>$password))->row();
-            if($Class!=null)
-            {
-                $Class["role"]="Class";
+        } else {
+            $Class = $this->db->get_where("class", array("username" => $username, "password" => $password))->row();
+            if ($Class != null) {
+                $Class["role"] = "Class";
 
                 return $Class;
-            }
-            else{
+            } else {
                 return null;
             }
         }
     }
 }
-
-
-
-?>
